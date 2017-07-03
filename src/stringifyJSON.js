98d2aa2b2d00bc5b0eb3fe40f92d42e6c7966604
Hack Reactor Student <student@hackreactor.com>
2017-07-03 15:30:21 -0700

    finished stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,83 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  
+  if(obj === null){
+    return 'null';  
+  }
+
+  if(obj === undefined || obj instanceof Function){
+    return '';
+  }
+
+  if(typeof obj === 'string') {
+    return '"' + obj.toString() + '"' ;
+
+  } else if(typeof obj === 'number' || typeof obj === 'boolean') {
+    return "" + obj;
+
+  } else if(Array.isArray(obj)) {
+
+    var string = '[';
+
+    if(obj.length === 0) {
+      return '[]';
+    }
+
+    if(obj.length === 1) {
+      return '[' + stringifyJSON(obj[0]) + ']';
+
+    }else{
+      
+      for(var i = 0; i< obj.length; i++){
+
+        if(i === obj.length-1){
+
+          string += stringifyJSON(obj[i]);
+
+        }else{
+          string += stringifyJSON(obj[i]) + ",";
+        }
+      }
+    }
+    return string + ']';
+
+  } else if(obj.constructor === Object){
+    var keylist = Object.keys(obj);
+    var string2 = '{';
+  
+    if (keylist.length === 0){
+
+      return '{}';
+
+    } else if (keylist.length === 1){
+
+      for(var  j in obj){
+
+        string2 += '"' + j + '":' + stringifyJSON(obj[j]); 
+      }
+
+    }else{
+
+      for(var k in obj){
+
+
+        if(obj[k] === undefined || obj[k] instanceof Function){
+
+          string2 += stringifyJSON(obj[k]);
+        } else if (k === keylist[keylist.length-1]){
+
+          string2 += '"' + k + '":' + stringifyJSON(obj[k]);
+        }else{
+
+          string2 += '"' + k + '":' + stringifyJSON(obj[k]) + ',';
+        }
+
+      }
+    }
+      
+    return string2 + '}';      
+
+  }
+
 };
