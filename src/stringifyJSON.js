25d149f3c407f6c8442405d6d42263386e332aca
Hack Reactor Student <student@hackreactor.com>
2017-07-03 15:43:42 -0700

    pomander fix

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,73 +5,73 @@
 
 var stringifyJSON = function(obj) {
   
-  if(obj === null){
+  if (obj === null) {
     return 'null';  
   }
 
-  if(obj === undefined || obj instanceof Function){
+  if (obj === undefined || obj instanceof Function) {
     return '';
   }
 
-  if(typeof obj === 'string') {
-    return '"' + obj.toString() + '"' ;
+  if (typeof obj === 'string') {
+    return '"' + obj.toString() + '"';
 
-  } else if(typeof obj === 'number' || typeof obj === 'boolean') {
-    return "" + obj;
+  } else if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return '' + obj;
 
-  } else if(Array.isArray(obj)) {
+  } else if (Array.isArray(obj)) {
 
     var string = '[';
 
-    if(obj.length === 0) {
+    if (obj.length === 0) {
       return '[]';
     }
 
-    if(obj.length === 1) {
+    if (obj.length === 1) {
       return '[' + stringifyJSON(obj[0]) + ']';
 
-    }else{
+    } else {
       
-      for(var i = 0; i< obj.length; i++){
+      for (var i = 0; i < obj.length; i++) {
 
-        if(i === obj.length-1){
+        if (i === obj.length - 1) {
 
           string += stringifyJSON(obj[i]);
 
-        }else{
-          string += stringifyJSON(obj[i]) + ",";
+        } else {
+          string += stringifyJSON(obj[i]) + ',';
         }
       }
     }
     return string + ']';
 
-  } else if(obj.constructor === Object){
+  } else if (obj.constructor === Object) {
     var keylist = Object.keys(obj);
     var string2 = '{';
   
-    if (keylist.length === 0){
+    if (keylist.length === 0) {
 
       return '{}';
 
-    } else if (keylist.length === 1){
+    } else if (keylist.length === 1) {
 
-      for(var  j in obj){
+      for (var j in obj) {
 
         string2 += '"' + j + '":' + stringifyJSON(obj[j]); 
       }
 
-    }else{
+    } else {
 
-      for(var k in obj){
+      for (var k in obj) {
 
 
-        if(obj[k] === undefined || obj[k] instanceof Function){
+        if (obj[k] === undefined || obj[k] instanceof Function) {
 
           string2 += stringifyJSON(obj[k]);
-        } else if (k === keylist[keylist.length-1]){
+        } else if (k === keylist[keylist.length - 1]) {
 
           string2 += '"' + k + '":' + stringifyJSON(obj[k]);
-        }else{
+        } else {
 
           string2 += '"' + k + '":' + stringifyJSON(obj[k]) + ',';
         }
